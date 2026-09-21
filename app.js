e13cbe801b61535f03a81f7c897094f5eb5aad1f
GTorralvo <gamestorralvo002@gmail.com>
2026-09-21 10:12:00 -0300

    inicamos a batalha

--- app.js
+++ app.js
@@ -113,5 +113,25 @@ function proximoTurno(){
 }
 
 const vidaGuerreiro = document.getElementById("vida-guerreiro")
-
 vidaGuerreiro.innerText = guerreiro.vida
+
+const ataqueGuerreiro = document.getElementById("ataque-guerreiro")
+ataqueGuerreiro.innerText = guerreiro.ataque
+
+const guerreiroAtacar = document.getElementById("guerreiro-atacar")
+guerreiroAtacar.addEventListener("click", () =>{
+    guerreiro.causaDano(mago)
+    vidaMago.innerText = mago.vida
+})
+
+const vidaMago = document.getElementById("vida-mago")
+vidaMago.innerText = mago.vida
+
+const ataqueMago = document.getElementById("ataque-mago")
+ataqueMago.innerText = mago.ataque
+
+const magoAtacar = document.getElementById("mago-atacar")
+magoAtacar.addEventListener("click", () =>{
+    mago.causaDano(guerreiro)
+    vidaGuerreiro.innerText = guerreiro.vida
+})
